b1804405d02cb06da92fbcf90db138de80b06735
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-11-07 18:11:13 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,11 +2,13 @@ class App < Sinatra::Base
   get "/" do
     erb :user_input
   end
+<<<<<<< HEAD
 
   post "/piglatinize" do
-    @translator = PigLatinizer.new
-    @user_phrase = params[:user_phrase]
+    @piglatin = PigLatinizer.new.piglatinize(params[:user_phrase])
 
     erb :piglatinize
   end
+=======
+>>>>>>> b7c5d727ac1262b6bb356ef00c964fb18a280d1e
 end
--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,4 +1,5 @@
 class PigLatinizer
+<<<<<<< HEAD
   def piglatinize(str)
     alpha = ('a'..'z').to_a
     vowels = %w[a e i o u]
@@ -17,5 +18,26 @@ class PigLatinizer
         str[1..-1] + str[0] + 'ay'
       end
     end.join(" ")
+=======
+  def piglatinize(word)
+    alpha = ('a'..'z').to_a
+    vowels = %w[a e i o u]
+    consonants = alpha - vowels
+    test_word = word.downcase
+
+    if vowels.include?(test_word[0])  #pass
+      word + 'way'
+    elsif consonants.include?(test_word[0]) && consonants.include?(test_word[1])
+      puts word
+      puts "second condition"
+      word[2..-1] + word[0..1] + 'ay'
+    elsif consonants.include?(test_word[0])
+      puts word
+      puts "third condition"
+      word[1..-1] + word[0] + 'ay'
+    else
+      word
+    end
+>>>>>>> b7c5d727ac1262b6bb356ef00c964fb18a280d1e
   end
 end
