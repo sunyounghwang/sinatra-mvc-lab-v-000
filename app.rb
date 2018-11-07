28b8a9ed964b3fa6a1ac490a8eea56452f2b6637
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-11-07 18:11:40 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,13 +2,10 @@ class App < Sinatra::Base
   get "/" do
     erb :user_input
   end
-<<<<<<< HEAD
 
   post "/piglatinize" do
     @piglatin = PigLatinizer.new.piglatinize(params[:user_phrase])
 
     erb :piglatinize
   end
-=======
->>>>>>> b7c5d727ac1262b6bb356ef00c964fb18a280d1e
 end
